93e048e0602f5e32793108ac05f464cf4d58a5e7
Mitsumi <pannierjp@gmail.com>
2019-06-11 13:04:40 +0200

    modif: css, pageStaff et jQuery
    Modification de page _index, _staff, _community.
    ajout: Leader Team A
    modif: Staff vers Team B
    modif: Team A vers Staff
    modifcations: Page staff: fonctionnaliter "click" pour faire apparaitre panneau.
    quand ouvert au survole panneau disparaît
    ajout: fond au carte des joueurs et au titre en transformation.

--- js/main.js
+++ js/main.js
@@ -21,7 +21,7 @@ window.onload = function() {
 		$(dropdown).hide();
 
 		$(btnRule).mouseover('on', function() {
-			$(dropdown).slideToggle();
+			$(dropdown).show();
 		});
 
 		var carouselImg = [
@@ -56,5 +56,120 @@ window.onload = function() {
         
         }; nextImg();
 
+        //La Page Staff
+        var staff = $('.fondateur, .sO');
+        var teamA = $('.sA');
+        var teamB = $('.sB');
+        var teamC = $('.sC');
+        $(staff).hide();
+        $(teamA).hide();
+        $(teamB).hide();
+        $(teamC).hide();
+
+        //Staff Panel
+        $('.overS').on('click', function() {
+        	$(staff).slideDown( function() {
+        		$('.sO .idBloc, .fondateur').css({
+        			backgroundImage: 'url("../images/bg/bgOrtest.png")',
+        			backgroundSize: 'cover'
+        		});
+        	});
+        	$('.overS').css({
+        		boxShadow: '0px 0px 5px 1px gray',
+        		backgroundImage: 'url("../images/bg/1.png")',
+        		backgroundSize: 'cover'
+        	});
+        });
+
+        $('.overS').on('mouseover', function() {
+        	$(staff).slideUp(1500, function() {
+        		$('.overS').css({
+        		backgroundImage: 'none',
+        		backgroundColor: 'rgba(0,0,0,0)',
+        		boxShadow: 'none'
+        		});
+        	});
+        });
+
+        //Team Alpha
+        $('.overA').on('click', function() {
+        	$(teamA).show(1500, function() {
+        		$('.sA .idBloc').css({
+        			backgroundImage: 'url("../images/bg/bgOrtest.png")',
+        			backgroundSize: 'cover'
+        		});
+        	}).fadeIn();
+        	$('.overA').css({
+        		boxShadow: '0px 0px 5px 1px gray',
+        		backgroundImage: 'url("../images/bg/1.png")',
+        		backgroundSize: 'cover'
+        	});
+        });
+
+        $('.overA').on('mouseover', function() {
+        	$(teamA).hide(1500, function() {
+        		$('.overA').css({
+        		backgroundImage: 'none',
+        		backgroundColor: 'rgba(0,0,0,0)',
+        		boxShadow: 'none'
+        		});
+        	}).fadeOut();
+        });
+
+        //Team Bravo
+        $('.overB').on('click', function() {
+        	$(teamB).show(1500, function() {
+        		$('.sB .idBloc').css({
+        			backgroundImage: 'url("../images/bg/rondale.png")',
+        			backgroundSize: 'cover'
+        		});
+        		$('.sB h4').css('color', 'goldenrod');
+        		$('.sB h5').css('color', 'gold');
+        		$('.sB p').css('color', 'white');
+        	});
+        	$('.overB').css({
+        		boxShadow: '0px 0px 5px 1px gray',
+        		backgroundImage: 'url("../images/bg/1.png")',
+        		backgroundSize: 'cover'
+        	});
+        });
+
+        $('.overB').on('mouseover', function() {
+        	$(teamB).hide(1500, function() {
+        		$('.overB').css({
+        		backgroundImage: 'none',
+        		backgroundColor: 'rgba(0,0,0,0)',
+        		boxShadow: 'none'
+        		});
+        	});
+        });
+
+        //Team Charlie
+        $('.overC').on('click', function() {
+        	$(teamC).show(1500, function() {
+        		$('.sC .idBloc').css({
+        			backgroundImage: 'url("../images/bg/bg.png")',
+        			backgroundSize: 'cover'
+        		});
+        		$('.sC h4').css('color', 'gold');
+        		$('.sC h5').css('color', 'navy');
+        		$('.sC p').css('color', 'white');
+        	});
+        	$('.overC').css({
+        		boxShadow: '0px 0px 5px 1px gray',
+        		backgroundImage: 'url("../images/bg/1.png")',
+        		backgroundSize: 'cover'
+        	});
+        });
+
+        $('.overC').on('mouseover', function() {
+        	$(teamC).hide(1500, function() {
+        		$('.overC').css({
+        		backgroundImage: 'none',
+        		backgroundColor: 'rgba(0,0,0,0)',
+        		boxShadow: 'none'
+        		});
+        	});
+        });
 	});
 }
